48a00544805e24a8454144201c9d857792ee63a8
Mubarak Mohideen N <mubarak.n@colanonline.com>
2017-01-27 20:20:32 +0530

    circle ci integrated

---
+++ app/src/main/java/com/learn/firebasesample/MultiDragActivity.java
@@ -0,0 +1,13 @@
+package com.learn.firebasesample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiDragActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_drag);
+    }
+}
